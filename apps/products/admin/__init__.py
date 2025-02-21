e5309ac5a7e4e078e5fe0b4fe3a3331ad17c6cd4
Sazzad Hossen <mshossen75@gmail.com>
2025-02-21 22:37:15 +0600

    feat: added brand model

--- apps/products/admin/__init__.py
+++ apps/products/admin/__init__.py
@@ -1,3 +1,4 @@
+from apps.products.admin.brand_admin import BrandAdmin
 from apps.products.admin.category_admin import CategoryAdmin
 from apps.products.admin.sub_category_admin import SubCategoryAdmin
 from apps.products.admin.unit_admin import (
@@ -10,4 +11,5 @@ __all__ = [
     "SubCategoryAdmin",
     "UnitAttributeAdmin",
     "UnitAttributeValueAdmin",
+    "BrandAdmin",
 ]
